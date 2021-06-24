c0116caa6f59b4ffd79ed9cd565a4a8eb34f1815
Jami Kousa <jami.kousa@helsinki.fi>
2021-06-24 16:17:44 +0300

    logger

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,45 +1,55 @@
-const { TOKEN } = require('./config')
+const logger = require("./logger");
+const { TOKEN } = require("./config");
 
 const tokenMiddleware = (req, res, next) => {
-  if (!TOKEN) return next()
+  if (!TOKEN) return next();
 
-  if (req.query.token !== TOKEN && req.headers['token'] !== TOKEN) {
-    logger.error({ message: `No token ${req.query.token}, ${req.headers['token']}` })
+  if (req.query.token !== TOKEN && req.headers["token"] !== TOKEN) {
+    logger.error({
+      message: `No token ${req.query.token}, ${req.headers["token"]}`,
+    });
 
-    return res.sendStatus(401)
+    return res.sendStatus(401);
   }
 
-  next()
-}
+  next();
+};
 
 /**
  * Ensures all fields are filled. The "template" is used as the basis for all emails and the email details then would overwrite the fields in template.
  * Validation checks that whatever is not in template is defined in emails
  */
 const validationMiddleware = (req, res, next) => {
-  const REQUIRED_FIELDS = ['to', 'subject', 'text']
+  const REQUIRED_FIELDS = ["to", "subject", "text"];
 
-  const { emails, template } = req.body
-  if (!Array.isArray(emails)) return res.status(400).send('body.emails must be an array')
-  if (!emails.length) return res.status(400).send('body.emails must have something in it')
-  if (!template || typeof template !== 'object') return res.status(400).send('body.template must be an object')
+  const { emails, template } = req.body;
+  if (!Array.isArray(emails))
+    return res.status(400).send("body.emails must be an array");
+  if (!emails.length)
+    return res.status(400).send("body.emails must have something in it");
+  if (!template || typeof template !== "object")
+    return res.status(400).send("body.template must be an object");
 
-  let missingFields = REQUIRED_FIELDS
-  if (template) Object.keys(template).forEach(key => missingFields = missingFields.filter(field => field === key))
+  let missingFields = REQUIRED_FIELDS;
+  if (template)
+    Object.keys(template).forEach(
+      (key) => (missingFields = missingFields.filter((field) => field === key))
+    );
 
-  let isMissingFields = false
-  emails.forEach(emailRequest => {
-    missingFields.forEach(field => {
-      if (Object.keys(emailRequest).includes(field)) isMissingFields = true
-    })
+  let isMissingFields = false;
+  emails.forEach((emailRequest) => {
+    missingFields.forEach((field) => {
+      if (Object.keys(emailRequest).includes(field)) isMissingFields = true;
+    });
   });
-  
-  if (isMissingFields) return res.status(400).send('does not contain all data to send mail') 
 
-  next()
-}
+  if (isMissingFields)
+    return res.status(400).send("does not contain all data to send mail");
+
+  next();
+};
 
 module.exports = {
   tokenMiddleware,
   validationMiddleware,
-}
+};
